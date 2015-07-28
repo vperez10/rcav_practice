be8ba33dc5f977e8b2f90af997568119f47e6ddb
vperez10 <vperez@chicagobooth.edu>
2015-07-28 10:27:20 -0400

    Working on RCAV practice

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,8 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+  	render("application.html.erb")
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:root", { :controller => "calculations", :action => "square_root" })
 end
