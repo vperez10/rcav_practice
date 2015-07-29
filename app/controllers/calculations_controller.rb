314f131821b28dca84dd78b62011f29e42c3f90a
vperez10 <vperez@chicagobooth.edu>
2015-07-29 15:23:14 -0500

    Working on routes for RCAV

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,7 +3,22 @@ class CalculationsController < ApplicationController
     render("instructions.html.erb")
   end
 
+def square
+	@square == params["square"] 
+  	render("square.html.erb")
+end
+
   def square_root
+  	@square_root == params["root"] 
+  	render("square_root.html.erb")
+end
+
+def range
+  	render("application.html.erb")
+end
+
+def payment
+	@payment == params["values"] 
   	render("application.html.erb")
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:root", { :controller => "calculations", :action => "square" })
   get("/square_root/:root", { :controller => "calculations", :action => "square_root" })
+  get("/random/:min/:max", { :controller => "calculations", :action => "range" })
+  get("/payment/:values", { :controller => "calculations", :action => "payment" })
 end
